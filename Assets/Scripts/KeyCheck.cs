dfa37746c9a728f7e7e017175b8ce6ee9c8c3bb0
Hannes Svahn <hannessvahn1999@gmail.se>
2022-02-22 09:01:56 +0100

    as

---
+++ Assets/Scripts/KeyCheck.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCheck : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    [SerializeField]
+    InventoryItemData IID;
+    
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (CheckForItem())
+        {
+            Debug.Log("True");
+            Destroy(door);
+            InventorySystem.current.Remove(IID);
+            this.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
+            Destroy(this.GetComponent<BoxCollider2D>());
+        }
+        else
+        {
+            Debug.Log("false");
+        }
+    }
+
+    bool CheckForItem ()
+    {
+        return InventorySystem.current.Get(IID) != null;
+    }
+
+
+}
