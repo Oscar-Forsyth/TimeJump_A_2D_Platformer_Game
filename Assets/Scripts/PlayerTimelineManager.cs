d3ef31d0545a794aa74c36b1a2b214a41879cf9b
Hannes Svahn <hannessvahn1999@gmail.se>
2022-02-24 16:24:15 +0100

    Added functionality to change background when timejumping

--- Assets/Scripts/PlayerTimelineManager.cs
+++ Assets/Scripts/PlayerTimelineManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerTimelineManager : MonoBehaviour
 {
+
+    [SerializeField]
+    GameObject FutureCanvas;
+
+    [SerializeField]
+    GameObject PastCanvas;
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("FutureRoom"))
         {
             LevelManager.instance.setPlayerStatus(true);
+            FutureCanvas.SetActive(true);
+            PastCanvas.SetActive(false);
         }
         else if (col.gameObject.CompareTag("PastRoom"))
         {
             LevelManager.instance.setPlayerStatus(false);
+            FutureCanvas.SetActive(false);
+            PastCanvas.SetActive(true);
         }
 
     }
