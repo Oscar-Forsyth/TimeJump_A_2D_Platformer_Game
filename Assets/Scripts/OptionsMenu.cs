dccfce73e93db2a17ec5e3781a84440183d21c9a
Jonathan Stigson <jonathan.stigson@gmail.com>
2022-02-26 16:04:27 +0100

    Create prefab for pauseOverlay and update mainmenu to account for settings changes.

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audiomixer;
+    public Slider mSlider;
+    public Toggle fullS;
     public void SetVolume(float volume)
     {
         audiomixer.SetFloat("volume", volume);
@@ -14,5 +17,14 @@ public class OptionsMenu : MonoBehaviour
     public void setFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+    }
+
+    public void UpdateValues()
+    {
+        float value = 0;
+        audiomixer.GetFloat("volume", out value);
+        mSlider.value = value;
+
+        fullS.isOn = Screen.fullScreen;
     }
 }
