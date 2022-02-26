1fd025b0b6c81f8f571294f991f9cbdce5ec6343
Oscar Forsyth <44546586+goatlover@users.noreply.github.com>
2022-02-26 14:42:46 +0100

    worked on chapter 2 bugs

--- Assets/Scripts/seedScript.cs
+++ Assets/Scripts/seedScript.cs
@@ -16,10 +16,19 @@ public class seedScript : MonoBehaviour
         {
            
             Destroy(GameObject.FindWithTag("Tree"));
-            Vector3 treePos = new Vector3(0.5f, 24.25f, 10f);
+            Vector3 treePos = new Vector3(0f, 24.25f, 10f);
             Instantiate(tree, col.transform.position + treePos, Quaternion.identity);
         }
         
     }
-  
+    public void OnTriggerExit2D(Collider2D col)
+    {
+
+        if (col.CompareTag("Seed"))
+        {
+            Destroy(GameObject.FindWithTag("Tree"));
+        }
+
+    }
+
 }
